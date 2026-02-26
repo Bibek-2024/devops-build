f42fe6286544b6e0585320e7b34c018df0387636
Bibek Kumar <bibekkumarsahu2011@gmail.com>
2026-02-26 15:40:07 +0530

    test webhook02

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-docker build -t my-react-app:latest -f docker/Dockerfile .
+#docker build -t my-react-app:latest -f docker/Dockerfile .
+#From here new updated script for cicd flow test
+
+
+IMAGE_NAME=my-react-app
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+
+echo "Building Docker image..."
+docker build -t $IMAGE_NAME:latest .
+
+echo "Tagging image..."
+
+if [ "$BRANCH_NAME" == "dev" ]; then
+    docker tag $IMAGE_NAME:latest <dockerhub-username>/dev:latest
+elif [ "$BRANCH_NAME" == "master" ]; then
+    docker tag $IMAGE_NAME:latest <dockerhub-username>/prod:latest
+fi
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-docker-compose -f docker/docker-compose.yml up -d
+
+#docker-compose -f docker/docker-compose.yml up -d
+# From here real cicd script test
+
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+
+if [ "$BRANCH_NAME" == "dev" ]; then
+    IMAGE=<dockerhub-username>/dev:latest
+elif [ "$BRANCH_NAME" == "master" ]; then
+    IMAGE=<dockerhub-username>/prod:latest
+fi
+
+echo "Pulling image..."
+docker pull $IMAGE
+
+echo "Stopping old container..."
+docker rm -f react-app || true
+
+echo "Running new container..."
+docker run -d -p 80:80 --name react-app $IMAGE
